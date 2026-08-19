8a02a3eb8b6b261a001aaee0206c197056940bcb
jadielalves <jadielweb@gmail.com>
2026-08-19 17:10:37 -0300

    começando valição do formulario

---
+++ frontend/assets/js/cadastro.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form')
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const verify = document.getElementById('verify').value
+
+    // VALIDAÇÃO
+
+    if (name === ''){
+        
+    }
+})
