4f0ee355f03bacaf24c2af1cd6cf0809e2a0426f
jadielalves <jadielweb@gmail.com>
2026-08-19 16:56:14 -0300

    refazendo cadastro.js e api.js

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,10 +1,2 @@
-export function registerUser(user) {
-    return fetch("http://localhost:3000/usuarios", {
-        method: 'POST',
-        headers: {
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
-    })
-}
+
 
