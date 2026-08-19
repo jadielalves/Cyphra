98ae9ce403e9ab0fcf90023fedb71a2eb6ce38ff
jadielalves <jadielweb@gmail.com>
2026-08-19 20:08:08 -0300

    validacao nome e resolucao de bug de svg
    validação de nome vazio, arrumando o evento de submit para haver validação em tempo real e resolvendo bug do svg quando o erro aparece.

--- frontend/assets/js/cadastro.js
+++ frontend/assets/js/cadastro.js
@@ -3,15 +3,30 @@ import { createError, removeError } from "./utils.js"
 
 const form = document.getElementById('form')
 
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const verify = document.getElementById('verify')
+
+name.addEventListener("input", () => {
+    const valueName = name.value.trim()
+
+    if(valueName === ''){
+        createError(name, "Digite seu nome")
+        return
+    } else {
+        removeError(name)
+    }
+})
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    const verify = document.getElementById('verify').value
+
 
     // VALIDAÇÃO
+
+    // Nome
 
 })
